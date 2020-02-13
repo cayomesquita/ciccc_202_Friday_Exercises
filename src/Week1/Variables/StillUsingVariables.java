219ca4b6d35d817e93de79dd67a32abfdac9455c
Cayo <cayomesquita@gmail.com>
2020-02-13 11:59:31 -0800

    Add #12 assignment

---
+++ src/Week1/Variables/StillUsingVariables.java
@@ -0,0 +1,9 @@
+package Week1.Variables;
+
+public class StillUsingVariables {
+    public static void main(String[] args) {
+        int year = 2021;
+        String name = "Cayo Amorim";
+        System.out.printf("My name is %s and I'll graduate in %d.\n", name, year);
+    }
+}
